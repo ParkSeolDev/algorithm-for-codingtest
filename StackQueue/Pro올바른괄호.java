217bf0eb7bebcf8774288e2c8c0d87731fa8e561
ParkSeolDev <parkseol.dev@gmail.com>
2024-10-28 22:29:40 +0900

    Study Week 2 - 스택&큐

---
+++ StackQueue/Pro올바른괄호.java
@@ -0,0 +1,22 @@
+package StackQueue;
+
+import java.util.*; 
+
+public class Pro올바른괄호 {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
