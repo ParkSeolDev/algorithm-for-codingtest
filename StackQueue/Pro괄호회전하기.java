89d58a4b2fe6748af44d80e737d9e842bcc7a47b
ParkSeolDev <parkseol.dev@gmail.com>
2024-10-29 22:56:13 +0900

    Study Week 2 - 스택&큐

--- StackQueue/Pro괄호회전하기.java
+++ StackQueue/Pro괄호회전하기.java
@@ -3,10 +3,10 @@ package StackQueue;
 import java.util.*;
 
 public class Pro괄호회전하기 {
-    public int solution(String s1) {
-        ArrayList<Character> s = new ArrayList<>();
-        for(int k = 0; k < s1.length(); k++){
-            s.add(s1.charAt(k));
+    public int solution(String s) {
+        ArrayList<Character> sList = new ArrayList<>();
+        for(int k = 0; k < s.length(); k++){
+            sList.add(s.charAt(k));
         }
 
         int result = 0;
@@ -14,21 +14,21 @@ public class Pro괄호회전하기 {
         map.put(')','(');
         map.put('}','{');
         map.put(']','[');
-        for(int i = 0; i < s.size(); i++){
+        for(int i = 0; i < sList.size(); i++){
             Stack<Character> stack = new Stack<>();
-            for(int j = 0; j < s.size(); j++){
+            for(int j = 0; j < sList.size(); j++){
                 if(stack.isEmpty()){
-                    stack.push(s.get(j));
-                }else if(stack.peek() == map.get(s.get(j))){
+                    stack.push(sList.get(j));
+                }else if(stack.peek() == map.get(sList.get(j))){
                     stack.pop();
                 }else{
-                    stack.push(s.get(j));
+                    stack.push(sList.get(j));
                 }
             }
             if(stack.size() == 0)result++;
-            char temp = s.get(0);
-            s.remove(0);
-            s.add(temp);
+            char temp = sList.get(0);
+            sList.remove(0);
+            sList.add(temp);
         }
         return result;
     }
