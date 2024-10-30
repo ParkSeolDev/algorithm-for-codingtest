6531387251d642c823ab3b45fd6cdb0ce722008c
ParkSeolDev <parkseol.dev@gmail.com>
2024-10-30 23:00:56 +0900

    Study Week 2 - 스택&큐

---
+++ StackQueue/Pro짝지어제거하기.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+class Pro짝지어제거하기 {
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch)
+                stack.pop();
+            else
+                stack.push(ch);
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
